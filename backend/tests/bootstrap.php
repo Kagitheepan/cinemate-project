ce6e4e37fbac7c2a4b0de28f0c78d76117989093
Kagialpha <steefkagi@gmail.com>
2026-05-08 12:33:20 +0200

    Make backend test bootstrap CI compatible

--- backend/tests/bootstrap.php
+++ backend/tests/bootstrap.php
@@ -5,9 +5,18 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    $envFile = dirname(__DIR__).'/.env';
+    $testEnvFile = dirname(__DIR__).'/.env.test';
+
+    if (is_file($envFile)) {
+        (new Dotenv())->bootEnv($envFile);
+    } elseif (is_file($testEnvFile)) {
+        (new Dotenv())->load($testEnvFile);
+        $_SERVER['APP_ENV'] ??= 'test';
+        $_SERVER['APP_DEBUG'] ??= '0';
+    }
 }
 
-if ($_SERVER['APP_DEBUG']) {
+if ($_SERVER['APP_DEBUG'] ?? false) {
     umask(0000);
 }
