80b653b9373533a153389243026f3ee34dfa7c39
Kagitheepan <153726447+Kagitheepan@users.noreply.github.com>
2026-07-06 15:43:13 +0200

    Delete file unused

--- playground-1.mongodb.js
+++
@@ -1,8 +0,0 @@
-// Sélectionne la base MongoDB utilisée par docker-compose.yml
-use('cinemate_movies');
-
-// Affiche tous les logs de connexion
-db.connection_logs.find();
-
-// Affiche les choix cookies RGPD
-db.cookie_consents.find();
